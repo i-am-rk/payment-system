24078e8ae792b4cbc3d737ca5060b177ae292033
i-am-rk <dev.ravikiran.rk@gmail.com>
2021-03-26 19:45:34 +0530

    resource added

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtWidgets import (
     QApplication,
@@ -12,25 +12,16 @@ from PyQt5.QtWidgets import (
     QVBoxLayout
 )
 
-
 appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
 version = appctxt.build_settings["version"]
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Hellow World v" + version)
-        w = QWidget()
-        layout = QVBoxLayout()
-        btn = QPushButton("press")
-        btn.setIconSize(QSize(20, 20))
-        btn.setIcon(QIcon("../resources/256.png"))
-        layout.addWidget(btn)
-        w.setLayout(layout)
 
-        self.setCentralWidget(w)
-
-
+        btn = QPushButton("Pressed")
+        btn.setIconSize(QSize(20,20))
+        btn.setIcon(QIcon("resources/Icons/home-black.svg"))
+        self.setCentralWidget(btn)
 if __name__ == '__main__':
     window = MainWindow()
     window.show()
