ac7628c430414b60259415195cb6da5f19165624
i-am-rk <dev.ravikiran.rk@gmail.com>
2021-03-31 21:03:35 +0530

    ...

--- src/main/python/threads.py
+++ src/main/python/threads.py
@@ -13,8 +13,8 @@ import globalvariables as gv
 
 # initialize opencv video capture
 cap = cv.VideoCapture(0)
-cap.set(3, gv.FeedWidth) # set width of video capture
-cap.set(4, gv.FeedHeight)  # set height of video capture
+# cap.set(3, gv.FeedWidth) # set width of video capture
+# cap.set(4, gv.FeedHeight)  # set height of video capture
 
 # define signals class
 class FeedSignals(QObject):
@@ -26,12 +26,10 @@ class FeedSignals(QObject):
     cv_image = pyqtSignal(object)
     error = pyqtSignal(str)
     
-
 # define worker
 class FeedWorker(QRunnable):
     """
     Defines FeedWorker thread
-
     :param args: Arguments to make available to run thread
     :param kwargs: Keywords arguments to make available to run thread
     """
@@ -42,34 +40,18 @@ class FeedWorker(QRunnable):
         self.kwargs = kwargs
         self.signals = FeedSignals()
 
-        print("inside worker init")
     # define run slot
-
     @pyqtSlot()
     def run(self):
         """
         Initialize the runner function of worker
         """
-        # for n in range(10):
-        #     print(n)
-        # print("inside worker run")
         try:
             while gv.VideoFeedStatus:
                 ret, frame = cap.read()
                 rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
                 self.signals.cv_image.emit(rgb_img)
-
         except Exception as e:
             self.signals.error.emit(str(e))
-        
-        # def cv_to_pixmap(cvimage):
-        #     """Convert opencv image to QPixmap"""
-        #     print("cv_to_pixmap")
-        #     h, w, ch = cvimage.shape # get shape of image
-        #     bytes_per_line = ch * w
-        #     convert_to_Qt_format = QImage(cvimage, w, h, bytes_per_line, QImage.Format_RGB888)
-        #     return convert_to_Qt_format
-
-
 #endregion VIDEO THREAD
 ######################################################################
